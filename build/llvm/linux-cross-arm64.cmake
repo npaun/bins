b566ebb5f12902f4d728259d1ced28ac1e6ba142
Nicholas Paun <npaun@cloudflare.com>
2024-09-26 14:23:04 -0700

    Try to further clean this up

--- build/llvm/linux-cross-arm64.cmake
+++ build/llvm/linux-cross-arm64.cmake
@@ -1,6 +1,17 @@
+# Tell CMake to cross compile
+set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+# Set up Zig toolchain
 set(CMAKE_C_COMPILER "${CMAKE_CURRENT_LIST_DIR}/cc" -target aarch64-linux-gnu)
 set(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_LIST_DIR}/c++" -target aarch64-linux-gnu)
 set(CMAKE_AR "${CMAKE_CURRENT_LIST_DIR}/ar")
 set(CMAKE_RANLIB "${CMAKE_CURRENT_LIST_DIR}/ranlib")
+
+# Tell llvm it is being built for another platform
+set(LLVM_DEFAULT_TARGET_TRIPLE aarch64-linux-gnu)
+
+# Include basic config
+include("${CDMAKE_CURRENT_LIST_DIR}/common.cmake")
+
